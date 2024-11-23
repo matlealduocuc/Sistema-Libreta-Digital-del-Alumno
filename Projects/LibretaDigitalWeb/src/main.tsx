818c36b195fcb8859a05760f6426e9113884ab84
matlealduocuc <mat.leal@duocuc.cl>
2024-11-23 00:03:30 -0300

    feat - Prueba Babel Polyfill

--- Projects/LibretaDigitalWeb/src/main.tsx
+++ Projects/LibretaDigitalWeb/src/main.tsx
@@ -1,3 +1,8 @@
+import 'react-app-polyfill/ie11'; // Para compatibilidad con IE 11 (opcional)
+import 'react-app-polyfill/stable'; // Para compatibilidad con características modernas de JavaScript
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
--- Projects/LibretaDigitalWeb/vite.config.ts
+++ Projects/LibretaDigitalWeb/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
 import react from "@vitejs/plugin-react-swc";
+import babel from "@rollup/plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel({
+      babelHelpers: "runtime",
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
+      exclude: "node_modules/**",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -12,6 +20,7 @@ export default defineConfig({
   },
   build: {
     outDir: "dist",
+    target: ["es2015"],
   },
   base: "/",
 });
